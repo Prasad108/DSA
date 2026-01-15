14da92dc04a62ce6a3a593a25766c25be708db2e
Prasad Ashok Dukale <prasad.dukale@atos.net>
2026-01-15 19:09:43 +0530

    Time: 115 ms (34.05%) | Memory: 47 MB (18.21%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,19 +1,24 @@
 class Solution {
     private int ans;
     public int findTargetSumWays(int[] nums, int target) {
-        dp(nums,0,target,0);
-        return ans;
+        Map<String, Integer> memo = new HashMap<>();
+        return dp(nums,0,target,0,memo);
     }
 
-    private void dp(int[] nums, int currIdx, int target, int currSum){
-        // System.out.println("currIdx:"+currIdx+", currSum:"+currSum);
-        if(currIdx==nums.length && currSum==target){
-                ans++;
-                return;
-        }
-            if(currIdx<nums.length){
-                dp(nums,currIdx+1,target,currSum+nums[currIdx]);
-                dp(nums,currIdx+1,target,currSum-nums[currIdx]);
+    private int dp(int[] nums, int currIdx, int target, int currSum, Map<String, Integer> memo){
+        if(currIdx==nums.length ){
+            if(currSum==target){
+                return 1;
             }
+                return 0;
+        }
+            String key= currIdx+","+currSum;
+            if(memo.containsKey(key)){
+                return memo.get(key);
+            }
+            int add=dp(nums,currIdx+1,target,currSum+nums[currIdx],memo);
+            int sub=dp(nums,currIdx+1,target,currSum-nums[currIdx],memo);
+            memo.put(key,add+sub);
+            return memo.get(key); 
     }
 }
